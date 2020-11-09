11e713e0904d990aca1354458511ddf25d38cf12
outsider4444 <danila_kuzin_2001@mail.ru>
2020-11-09 21:04:53 +0400

    удален секретный код

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*j91ue!5n2im!x+f50893r-din3+km&433p0cuhql-sy@5spny'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

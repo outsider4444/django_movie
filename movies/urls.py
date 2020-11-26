ac53b7aa49cc08d7b68ec416d3b42d6f50978a99
outsider4444 <danila_kuzin_2001@mail.ru>
2020-11-26 16:48:50 +0400

    Добавлены страницы актеров и режиссеров

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
    path("", views.MovieView.as_view(), name='movies'),
    path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
-   path("review/<int:pk>/", views.AddReview.as_view(), name='add_review')
+   path("review/<int:pk>/", views.AddReview.as_view(), name='add_review'),
+   path("actor/<str:slug>/", views.ActorView.as_view(), name='actor_detail'),
 ]
